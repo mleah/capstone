e82d38aed667d87a623e1005c8a7d8097c912eba
mleah <mariah.hanson@detroitlabs.com>
2015-05-06 11:38:52 -0400

    building overwritecontinuegame partial, researching formulas for distance between lat and long points

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -4,19 +4,28 @@
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
         when('/', {
+            controller: 'testController',
             templateUrl: 'partials/landingscreen.html'
         }).
         when('/newgamewelcome', {
+            controller: 'testController',
             templateUrl: 'partials/newgamewelcomescreen.html'
         }).
         when('/about', {
+            controller: 'testController',
             templateUrl: 'partials/about.html'
         }).
         when('/maingamescreen', {
+            controller: 'testController',
             templateUrl: 'partials/licenseplatemain.html'
         }).
         when('/continuegamewelcome', {
+            controller: 'testController',
             templateUrl: 'partials/continuegamewelcomescreen.html'
+        }).
+        when('/newgameoverwrite', {
+            controller: 'testController',
+            templateUrl: 'partials/newgameoverwriteoldgame.html'
         }).
         otherwise({
             redirectTo: '/'
--- app/javascript/controllers.js
+++ app/javascript/controllers.js
@@ -1,29 +1,37 @@
 (function() {
-  var licenseControllers = angular.module('licenseControllers', []);
+    var licenseControllers = angular.module('licenseControllers', []);
 
-  licenseControllers.controller('licenseListCtrl', [function() {
-    var vm = this;
-    //hardcode some for now
-    vm.states = [{abbreviation: 'AL', 
-                  fullname: 'Alabama',
-            }, {abbreviation: 'AK', 
-                  fullname: 'Alaska',
-            },{abbreviation: 'AZ', 
-                  fullname: 'Arkansas',
-            }];
-  }]);
+    licenseControllers.controller('licenseListCtrl', [function() {
+        var vm = this;
+        //hardcode some for now
+        vm.states = [{
+            abbreviation: 'AL',
+            fullname: 'Alabama',
+        }, {
+            abbreviation: 'AK',
+            fullname: 'Alaska',
+        }, {
+            abbreviation: 'AZ',
+            fullname: 'Arkansas',
+        }];
+    }]);
 
-  // licenseControllers.controller('testCtrl', ['', function() {
-  //   var vm = this;
-  //   vm.whatisfactorydoing? = FACTORYNAME somethign here;
-  // }]);
+    // licenseControllers.controller('testCtrl', ['', function() {
+    //   var vm = this;
+    //   vm.whatisfactorydoing? = FACTORYNAME somethign here;
+    // }]);
 
-  licenseControllers.controller('testController', [function(){
-    var vm = this;
+    licenseControllers.controller('testController', ['$scope', '$location', function($scope, $location) {
+        var vm = this;
 
-    vm.speak = "Hello world!";
+        vm.speak = "Hello world!";
+        vm.location = '';
+        // vm.showHomeButton = function()
 
-  }]);
- 
+        // $scope.showHomeButton = $location.path() !== '/';
+        console.log($location.path());
+
+    }]);
+
 
 })();
