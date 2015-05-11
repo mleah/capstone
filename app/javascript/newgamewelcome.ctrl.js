f66949469a3c5f27905b6658c9597ce775d6ff32
mleah <mariah.hanson@detroitlabs.com>
2015-05-10 23:39:22 -0400

    finsihing for the night, building out more controllers

---
+++ app/javascript/newgamewelcome.ctrl.js
@@ -0,0 +1,28 @@
+(function() {
+    var newGameControllers = angular.module('newGameControllers', []);
+
+
+
+
+    newGameControllers.controller('newGameCtrl', [ function() {
+        var vm = this;
+
+        vm.speak = "Hello world!";
+        // vm.location = '';
+        // vm.showHomeButton = function()
+
+        // $scope.showHomeButton = $location.path() !== '/';
+        // console.log($location.path());
+
+
+
+    }]);
+
+
+    plateControllers.controller('plateProfileList', ['plateProfiles', function(plateProfiles) {
+        var self = this;
+        
+    }]);
+
+
+})();
